cfbc3b43919ae7934b4fac6822ff6545cb79653f
Mariana Shydlovska <marjanka77777@gmail.com>
2023-11-28 21:31:24 +0100

    Create js week02

---
+++ js/week02/script.js
@@ -0,0 +1,37 @@
+/*
+console.log("Hello, World!") //English
+console.log("Hej, Världen!") //Swedish
+console.log("Привіт, Світ!") //Ukrainian
+console.log("Привет, Mир!") //Russian
+*/
+
+/*
+console.log("I'm awesome");
+*/
+
+/*
+let x
+console.log("the value of my variable x will be: undefined")
+console.log(x)
+x = 30
+console.log("the value of my variable x will be: 30")
+console.log(x)
+*/
+
+/*
+let y = ""
+console.log("the value of x will be: ''")
+console.log(y)
+y = "Hello World!"
+console.log("the value of x will be: 'Hello World!'")
+console.log(y)
+*/
+
+/*
+const z = 7.25
+console.log(z)
+const a = Math.round(z)
+console.log(a)
+const f = Math.max(a, z)
+console.log(f)
+*/
