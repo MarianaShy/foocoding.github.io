3e81c5df45d4b71a8d1f42b79937dbd5eff8df8f
Mariana Shydlovska <marjanka77777@gmail.com>
2023-11-28 22:28:40 +0100

    add 8 step

--- js/week02/script.js
+++ js/week02/script.js
@@ -35,3 +35,58 @@ console.log(a)
 const f = Math.max(a, z)
 console.log(f)
 */
+
+/*
+let dogNames = []
+console.log("I think the value of the array is: undefined")
+console.log(dogNames)
+let myFavoriteAnimals = ["dog", "horse", "cat"]
+console.log(myFavoriteAnimals)
+myFavoriteAnimals.push ("baby pig")
+console.log(myFavoriteAnimals)
+*/
+
+/*let myString = "this is a test"
+console.log(myString)
+let myStringLength = myString.length
+console.log(myStringLength)
+*/
+
+/*
+let day = "Monday";
+console.log("The value of my variable day is: " + day);
+let date = 27;
+console.log("The value of my variable date is: " + date);
+let itIsSnowing = true
+console.log("The value of my variable itIsSnowing is: " + itIsSnowing);
+let temperature = 4;
+console.log("The value of my variable temperature is: " + temperature);
+let nothing;
+console.log("The value of my variable nuthing is: " + nothing);
+
+console.log("The type of my variable day is: string")
+console.log("The type of my variable date is: number")
+console.log("The type of my variable itIsSnowing is: boolean")
+console.log("The type of my variable temperature is: number")
+console.log("The type of my variable nothing is: undefined")
+
+console.log(typeof day)
+console.log(typeof date)
+console.log(typeof itIsSnowing)
+console.log(typeof temperature)
+console.log(typeof nothing)
+
+
+if (typeof day === typeof date) {
+    console.log('SAME TYPE');
+}
+else {
+  console.log('NOT THE SAME TYPE')
+}
+if (typeof date === typeof temperature) {
+    console.log('SAME TYPE');
+}
+else {
+  console.log('NOT THE SAME TYPE')
+}
+*/
