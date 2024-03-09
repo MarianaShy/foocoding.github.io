ec0d7ff92391b84b1538c7fd3a2f91fd21fbdfa9
marianashy <marjanka77777@gmail.com>
2024-03-09 10:00:59 +0100

    ad readline

--- node/node3/CLI1/cli.js
+++ node/node3/CLI1/cli.js
@@ -11,15 +11,23 @@ function instructions () {
 	console.log("                    Commands:");
 	console.log("getAll ----------------------- Display your To Do list;");
 	console.log("post <task> ------------------ Create new To Do task;");
-	console.log("change <id> <task> <status>--- Change chosen task;");
+	console.log("change <id> <task> ----------- Change chosen task;");
 	console.log("delete <id> ------------------ Delete chosen task;");
 	console.log("end -------------------------- Quit the application;");
  }
  
  instructions();
 
+// call readline
+ var rl = readline.createInterface(process.stdin, process.stdout);
 
- 
+
+// Display a prompt to the user
+
+rl.setPrompt("Your command: ");
+rl.prompt();
+
+
 async function test() {
 	const testInst = await getAll()
 console.log(testInst)}
