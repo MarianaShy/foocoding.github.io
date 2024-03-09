8588700f278a87e32556861212168a8818325c28
marianashy <marjanka77777@gmail.com>
2024-03-09 09:55:57 +0100

    add instructions

--- node/node3/CLI1/cli.js
+++ node/node3/CLI1/cli.js
@@ -1,10 +1,25 @@
+//import modules
+
 const readline = require("readline");
 
 const getAll =  require("./instructions.js")
 
-console.log("Hello world" )
+
+// Display instructions on how to use each command
+
+function instructions () {
+	console.log("                    Commands:");
+	console.log("getAll ----------------------- Display your To Do list;");
+	console.log("post <task> ------------------ Create new To Do task;");
+	console.log("change <id> <task> <status>--- Change chosen task;");
+	console.log("delete <id> ------------------ Delete chosen task;");
+	console.log("end -------------------------- Quit the application;");
+ }
+ 
+ instructions();
 
 
+ 
 async function test() {
 	const testInst = await getAll()
 console.log(testInst)}
