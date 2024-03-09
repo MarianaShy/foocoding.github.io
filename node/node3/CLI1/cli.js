8b7bdd7390e909ac6d90e9c6552bd89305377d78
marianashy <marjanka77777@gmail.com>
2024-03-09 22:33:58 +0100

    add get All command

--- node/node3/CLI1/cli.js
+++ node/node3/CLI1/cli.js
@@ -9,31 +9,37 @@ const getAll =  require("./instructions.js")
 
 function instructions () {
 	console.log("                    Commands:");
-	console.log("getAll ----------------------- Display your To Do list;");
+	console.log("To do list ----------------------- Display your To Do list;");
 	console.log("post <task> ------------------ Create new To Do task;");
 	console.log("change <id> <task> ----------- Change chosen task;");
 	console.log("delete <id> ------------------ Delete chosen task;");
+	
 	console.log("end -------------------------- Quit the application;");
  }
  
  instructions();
 
 // call readline
- var rl = readline.createInterface(process.stdin, process.stdout);
+ const rl = readline.createInterface(
+	{input: process.stdin, 
+	output: process.stdout});
 
 
-// Display a prompt to the user
+// Display prompt
 
 rl.setPrompt("Your command: ");
 rl.prompt();
 
-
-rl.on("line", async function test() {
-	const testInst = await getAll()
-console.log(testInst)
-test()
+//set functions to commands
+rl.on("line", async function  (input) {
+	if(input === "To do list"){
+	const answer = await getAll()
+	console.log(answer)
+	}
+	else
+	console.log("no input")
 }
-)
+ );
 
 
 
--- node/node3/CLI1/instructions.js
+++ node/node3/CLI1/instructions.js
@@ -1,7 +1,16 @@
-function getAll() {
-	console.log("Instructions")
-	const test = 1
-	return test
+// define functions
+
+async function getAll() {
+	try {
+		const response = await fetch("http://localhost:3000/todos");
+		if (!response.ok) {
+		  throw new Error("Can not get data from server");
+		}
+		const toDoList = await response.json();
+		return toDoList;
+	 } catch (error) {
+		console.error(error);
+	 }
 }
 
 
